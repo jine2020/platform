f03be35b8011ebc07c814bf390ae4eabd5fe068e
lenovo <306262361@qq.com>
2020-08-23 00:49:37 +0800

    增加测试用例页面

--- demo_platform/src/App.vue
+++ demo_platform/src/App.vue
@@ -26,31 +26,19 @@
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
       <router-view></router-view>
-      <Login/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Login from './components/Login'
 export default {
   name: 'App',
 
   components: {
-    Login,
   },
 
   data: () => ({
--- demo_platform/src/routes.js
+++ demo_platform/src/routes.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from './components/Login'
+import DashBoard from './components/DashBoard'
 
 Vue.use(VueRouter)
 
 const routes = [
-    { path: '/login', components: Login },
+    { path: '/', component: Login },
+    { path: '/login', component: Login },
+    { path: '/dashboard', component: DashBoard },
 ]
 
 const router = new VueRouter({
